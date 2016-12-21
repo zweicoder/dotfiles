5c5a37bfc694625965d8b8c489a963b3aa1ab472
zweicoder <zweicoder@gmail.com>
2016-12-21 16:34:59 +0800

    Fix docker-compose installation script

--- dotfiles/provision/install-docker-compose.sh
+++ dotfiles/provision/install-docker-compose.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Install docker-compose
-sudo -i
-curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /tmp/docker-compose
+sudo cp /tmp/docker-compose /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
