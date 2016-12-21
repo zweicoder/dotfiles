43d719dee9a1d36ae49143ff6f214d2aeea5fdcb
zweicoder <zweicoder@gmail.com>
2016-12-21 16:16:47 +0800

    Add provisioning scripts

---
+++ dotfiles/provision/README.md
@@ -0,0 +1,7 @@
+Provisioning scripts to quickly install things on a remote instance.
+
+Usage:
+
+```
+ssh <user>@<host> 'bash -s' < ./install-something.sh
+```
---
+++ dotfiles/provision/install-docker-compose.sh
@@ -0,0 +1,4 @@
+# Install docker-compose
+sudo -i
+curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
