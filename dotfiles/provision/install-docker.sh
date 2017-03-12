aa9a01e3329ed8f2a63822442e286e8c7807c78f
zweicoder <zweicoder@gmail.com>
2017-03-12 17:42:02 +0800

    Update docker installation script to install docker-ce

--- dotfiles/provision/install-docker.sh
+++ dotfiles/provision/install-docker.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-# Install docker on Ubuntu 16.04
-sudo apt-key adv \
-               --keyserver hkp://ha.pool.sks-keyservers.net:80 \
-               --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-
-echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
+# Install docker-ce on Ubuntu 16.04
 sudo apt-get update
-sudo apt-get install -y docker-engine
-sudo usermod -aG docker $USER
-# Login to docker group
-newgrp docker
+sudo apt-get install -y \
+        linux-image-extra-$(uname -r) \
+        linux-image-extra-virtual
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce
